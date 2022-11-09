e92011155300245e93dd2cfbe97481f0fdde30b1
Eduardo <chichastipanik@gmail.com>
2022-11-08 22:16:20 -0500

    test

--- sistema/settings.py
+++ sistema/settings.py
@@ -150,11 +150,11 @@ USE_TZ = True
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
-STATICFILES_DIR=[os.path.join(BASE_DIR,"static")]
+STATICFILES_DIRS=[os.path.join(BASE_DIR,'static')]
 
-MEDIA_ROOT = os.path.join(BASE_DIR,"static/imagenes")
+MEDIA_ROOT = os.path.join(BASE_DIR, 'static/imagenes')
 MEDIA_URL = '/imagenes/'
 
 
