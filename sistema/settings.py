4fe6626b2937f05101b13f4b6824ecac9c80cc17
Eduardo <chichastipanik@gmail.com>
2022-11-08 21:53:49 -0500

    test

--- sistema/settings.py
+++ sistema/settings.py
@@ -25,9 +25,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-+!8kxy9a($_bm!aj!+y0b-q-s+$7nc!w0c2#0@c1714a_oww_w'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['mascotas-xd.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
